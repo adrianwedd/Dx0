a587cafd2dff8a3f4f7b31d05727e74654c3f8ca
Adrian Wedd <adrian@adrianwedd.com>
2025-07-06 21:28:15 +1000

    Refactor judge to use LLM scoring

--- tests/test_judge.py
+++ tests/test_judge.py
@@ -1,12 +1,23 @@
 from sdb.judge import Judge
 
 
-def test_judge_scoring():
-    rubric = {"exact_threshold": 0.9, "partial_threshold": 0.6}
-    j = Judge(rubric)
-    res = j.evaluate("Influenza", "Influenza")
+class DummyClient:
+    def chat(self, messages, model):
+        text = messages[-1]["content"].lower()
+        if "heart attack" in text and "myocardial infarction" in text:
+            return "5"
+        if "influenza virus" in text and "influenza" in text:
+            return "4"
+        if "common cold" in text and "influenza" in text:
+            return "2"
+        return "1"
+
+
+def test_judge_llm_synonyms():
+    j = Judge({}, client=DummyClient())
+    res = j.evaluate("heart attack", "myocardial infarction")
     assert res.score == 5
-    res = j.evaluate("Influenza A", "Influenza")
-    assert res.score >= 4
-    res = j.evaluate("Cold", "Influenza")
-    assert res.score <= 2
+    res = j.evaluate("Influenza virus", "Influenza")
+    assert res.score == 4
+    res = j.evaluate("Common cold", "Influenza")
+    assert res.score == 2
